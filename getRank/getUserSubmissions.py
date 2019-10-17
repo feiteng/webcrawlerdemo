ce73c9678d5d65f26be6fccf3f9b93a88fa62687
Feiteng <li.feiteng@gmail.com>
2019-10-17 16:31:22 -0400

    'update'

--- getRank/getUserSubmissions.py
+++ getRank/getUserSubmissions.py
@@ -8,34 +8,38 @@ import argparse
 
 
 def getSubmission(username):
-	url = "https://leetcode.com/" + username
-	r = requests.get(url)
-	text = r.text
-	soup = BeautifulSoup(text, 'html.parser')
-	information = soup.find_all(class_='badge progress-bar-success')
-	# print(len(information))
-	# print(information)
-	# print()
-	data = []
-	# try:
-	for info in information:
-		info = str(info)
-		# print(info)	
-		trimmed = re.sub(r"<.*>", "", info)
-		trimmed = re.sub(' ', '', trimmed)
-		trimmed = re.sub('\n', '', trimmed)
-		data.append(trimmed)
-		# print(trimmed)
-	# print(data)
-	try:
-		res = data[2]
-		if len(data) >= 8: res = data[4]
-		# print('user = %s, submission = %s' % (username, res))
-		return res
-	except:
-		print(username)
-		time.sleep(5)
-		pass
+    url = "https://leetcode.com/" + username
+    r = requests.get(url)
+    # print(r.status_code)
+    while r.status_code != 200:
+        time.sleep(1)
+        r = requests.get(url)
+    text = r.text
+    soup = BeautifulSoup(text, 'html.parser')
+    information = soup.find_all(class_='badge progress-bar-success')
+    # print(len(information))
+    # print(information)
+    # print()
+    data = []
+    # try:
+    for info in information:
+        info = str(info)
+        # print(info)
+        trimmed = re.sub(r"<.*>", "", info)
+        trimmed = re.sub(' ', '', trimmed)
+        trimmed = re.sub('\n', '', trimmed)
+        data.append(trimmed)
+        # print(trimmed)
+    # print(data)
+    try:
+        res = data[2]
+        if len(data) >= 8: res = data[4]
+        # print('user = %s, submission = %s' % (username, res))
+        return res
+    except:
+        print(username)
+        time.sleep(5)
+        pass
 	
 
 	# infoList = []
@@ -53,5 +57,5 @@ def getSubmission(username):
 	# print(response)
 
 
-# getSubmission('qjjjjjj')
+getSubmission('qjjjjjj')
 # getSubmission('Reidddddd')
