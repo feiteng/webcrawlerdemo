83f868bc9eff19eb7a4a95f2aca179a584731609
Feiteng <li.feiteng@gmail.com>
2021-05-07 23:59:35 -0400

    added relative path

--- getRank/code/methods/parseCaller.py
+++ getRank/code/methods/parseCaller.py
@@ -5,16 +5,19 @@ import JPLAG
 import sys
 
 
-contest = 209
+contest = 236
 pages = 500
+
+dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+print(dir)
 
 
 # print("contest = %d .. pages = %d" % (contest, pages))
 
 try:
-	input = sys.argv
-	contest = int(input[1])
-	pages = int(input[2])
+	# input = sys.argv
+	# contest = int(input[1])
+	# pages = int(input[2])
 
 	print("contest = %d .. pages = %d" % (contest, pages))
 
@@ -23,7 +26,8 @@ try:
 	fetchContest_Code.parseJSON(contest)
 	print('Finished fetching submission..')
 	questionList = []
-	ContestSubmission = 'C:/Users/lifeiteng/projects/visualizer/getRank/Contest submission/'
+	# ContestSubmission = 'C:/Users/lifeiteng/projects/visualizer/getRank/Contest submission/'
+	ContestSubmission = dir + '/Contest submission/'
 	contestPath = ContestSubmission + '/' + str(contest) + '/cpp'
 
 	for folder in os.scandir(contestPath):
@@ -32,7 +36,8 @@ try:
 	JPLAG.jplag(contest)
 	print('Finished JPLAG..')
 
-	updateScriptFile = 'C:/Users/lifeiteng/projects/visualizer/getRank/code/update_indexMD.py'
+	# updateScriptFile = 'C:/Users/lifeiteng/projects/visualizer/getRank/code/update_indexMD.py'
+	updateScriptFile = dir + '/code/update_indexMD.py'
 	exec(open(updateScriptFile).read())
 
 
